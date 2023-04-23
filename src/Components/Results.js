dbb20ee04fe8ad6ce870b21bcf0b0c22a5f89e54
DestinyJoyner <destinyjoyner@pursuit.org>
2023-04-22 23:40:10 -0400

    still working

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,13 +1,26 @@
-import React from "react";
 import { useContextProvider } from "./Provider.js";
+import "./Results.css"
 
 export default function Results() {
 
   const { result, setResult, categories } = useContextProvider();
 
   // lbs of co2 -> 6000 - 16000 good, 16000-22000 avg, >22000 bad
-  return <div>
-    <h1>{result}</h1>
-    <h2>{result *12}</h2>
-  </div>;
+  return (
+    <div className="results">
+      <div className="results-chart">
+        <section className="monthly">
+          <h4>Monthly</h4>
+          <span>{result}</span>
+        </section>
+
+        <section className="monthly">
+          <h4>Yearly</h4>
+          <span>{result * 12}</span>
+        </section>
+
+      </div>
+
+  </div>
+  );
 }
