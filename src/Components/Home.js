cb208630f835870500d386c34c2831b6de273f12
minagrullon <minagrullon@pursuit.org>
2023-04-23 00:10:27 -0400

    css?

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,21 +1,22 @@
 import { Link } from "react-router-dom";
-import {FcGlobe} from "react-icons/fc"
-import homePage from "../assets/carbon-home.gif"
-import "./Home.css"
+import { FcGlobe } from "react-icons/fc";
+import homePage from "../assets/carbon-home.gif";
+import "./Home.css";
 
 function Home(props) {
-    return (
-        <div className='home'>
-        {/* <img src = {homePage} alt = "gif" /> */}
-        <button 
-        className="start">
-            <Link to = "/transportation">START</Link>
-        </button>
+  return (
+    <div className="home">
+      {/* <img src = {homePage} alt = "gif" /> */}
+      <button className="start">
+        <Link to="/transportation">Start the Quiz</Link>
+      </button>
 
-        {/* a tag to outside carbon link */}
-        <Link className= "link" to= "/">What is Carbon Footprint?</Link>
-        </div>
-    );
+      {/* a tag to outside carbon link */}
+      <Link className="link" to="/">
+        What is Carbon Footprint?
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
