fba96d75d313833163c3d809b0cdeaf499198eb8
bowersjuan <bowersjuan@gmail.com>
2023-04-23 08:19:54 -0400

    update and style Food and Transportaion components

--- src/Components/Food.js
+++ src/Components/Food.js
@@ -10,7 +10,7 @@ import {
   TbCarrot,
   TbBrandApple,
 } from "react-icons/tb";
-import { GiChicken, GiWheat } from "react-icons/gi";
+import { GiChicken, GiWheat, GiFarmer, GiRiver } from "react-icons/gi";
 import { BiBowlRice } from "react-icons/bi";
 
 import { useNavigate } from "react-router-dom";
@@ -67,35 +67,36 @@ export default function Food() {
   return (
     <div className="food" style={{ fontFamily: "monospace" }}>
       <h2>What food items do you consume?</h2>
-      <h4>Select all that apply!</h4>
+      <h4>
+        <em>Select all that apply!</em>
+      </h4>
+
       <form>
         <div className="food-selections">
-          <label htmlFor="beef">
-            <input
-              type="checkbox"
-              id="beef"
-              value={categories.food["Beef"]}
-              onChange={handleOptionChange}
-            />
-            <SiHappycow size="50" /> Beef
-          </label>
           <label htmlFor="chicken">
             <input
               type="checkbox"
               id="chicken"
               value={categories.food["Chicken"]}
               onChange={handleOptionChange}
-            />
-            <GiChicken size="50" /> Chicken
+            />{" "}
+            Chicken
+            <section>
+              <GiChicken size="50" />
+            </section>
           </label>
+
           <label htmlFor="pork">
             <input
               type="checkbox"
               id="pork"
               value={categories.food["Pork"]}
               onChange={handleOptionChange}
-            />
-            <TbPig size="50" /> Pork
+            />{" "}
+            Pork
+            <section>
+              <TbPig size="50" />
+            </section>
           </label>
 
           <label htmlFor="fish (wild)">
@@ -104,82 +105,138 @@ export default function Food() {
               id="fish (wild)"
               value={categories.food["Fish (wild)"]}
               onChange={handleOptionChange}
-            />
-            <TbFish size="50" /> Wild Fish
+            />{" "}
+            Wild Fish
+            <section>
+              <GiRiver size="50" />
+              <TbFish size="50" />
+            </section>
           </label>
+
           <label htmlFor="fish (farmed)">
             <input
               type="checkbox"
               id="fish (farmed)"
               value={categories.food["Fish (farmed)"]}
               onChange={handleOptionChange}
-            />
-            <TbFish size="50" /> Farmed Fish
+            />{" "}
+            Farmed Fish
+            <section>
+              <GiFarmer size="50" />
+              <TbFish size="50" />
+            </section>
           </label>
+
+          <label htmlFor="beef">
+            <input
+              type="checkbox"
+              id="beef"
+              value={categories.food["Beef"]}
+              onChange={handleOptionChange}
+            />{" "}
+            Beef
+            <section>
+              <SiHappycow size="50" />
+            </section>
+          </label>
+
           <label htmlFor="dairy (cheese)">
             <input
               type="checkbox"
               id="dairy (cheese)"
               value={categories.food["Dairy (cheese)"]}
               onChange={handleOptionChange}
-            />
-            <TbCheese size="50" /> Cheese
+            />{" "}
+            Cheese
+            <section>
+              <TbCheese size="50" />
+            </section>
           </label>
+
           <label htmlFor="dairy (milk)">
             <input
               type="checkbox"
               id="dairy (milk)"
               value={categories.food["Dairy (milk)"]}
               onChange={handleOptionChange}
-            />
-            <TbMilk size="50" /> Milk
+            />{" "}
+            Milk
+            <section>
+              <TbMilk size="50" />
+            </section>
           </label>
+
           <label htmlFor="eggs">
             <input
               type="checkbox"
               id="eggs"
               value={categories.food["Eggs"]}
               onChange={handleOptionChange}
-            />
-            <TbEggs size="50" /> Eggs
+            />{" "}
+            Eggs
+            <section>
+              <TbEggs size="50" />
+            </section>
           </label>
+
           <label htmlFor="rice">
             <input
               type="checkbox"
               id="rice"
               value={categories.food["Rice"]}
               onChange={handleOptionChange}
-            />
-            <BiBowlRice size="50" /> Rice
+            />{" "}
+            Rice
+            <section>
+              <BiBowlRice size="50" />
+            </section>
           </label>
+
           <label htmlFor="wheat">
             <input
               type="checkbox"
               id="wheat"
               value={categories.food["Wheat"]}
               onChange={handleOptionChange}
-            />
-            <GiWheat size="50" /> Wheat
+            />{" "}
+            Wheat
+            <section>
+              <GiWheat size="50" />
+            </section>
           </label>
+
           <label htmlFor="vegetables">
             <input
               type="checkbox"
               id="vegetables"
               value={categories.food["Vegetables"]}
               onChange={handleOptionChange}
-            />
-            <TbCarrot size="50" /> Veggies
+            />{" "}
+            Veggies
+            <section>
+              <TbCarrot size="50" />
+            </section>
           </label>
+
           <label htmlFor="fruits">
             <input
               type="checkbox"
               id="fruits"
               value={categories.food["Fruits"]}
               onChange={handleOptionChange}
-            />
-            <TbBrandApple size="50" /> Fruits
+            />{" "}
+            Fruits
+            <section>
+              <TbBrandApple size="50" />
+            </section>
           </label>
         </div>
+
+        <div className="food-image-container">
+          <div className="food-image"></div>
+        </div>
+        <p>Tip: Lower your Carbon 👣 by eating locally sourced food!</p>
+
         <button className="next" onClick={handleSubmit}>
           Next!
         </button>
